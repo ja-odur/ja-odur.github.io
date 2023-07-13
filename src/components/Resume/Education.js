f5475462b0856d49c6ca09e9508287407ca5cad0
ja-odur <odurjoseph8@gmail.com>
2023-07-13 20:36:13 +0300

    Fix Typo

--- src/components/Resume/Education.js
+++ src/components/Resume/Education.js
@@ -1,21 +1,23 @@
 function Education({educationBackground}) {
     return (
         <>
-            educationBackground &&
-            <>
-                <div className="resume-body-title"><span className="resume-body-pager"></span>Education</div>
-                {
-                    educationBackground.map((eBackground, eID) => {
-                        return (
-                            <div className="education-wrapper" key={eID}>
-                                <div className="education-level">{eBackground.course}</div>
-                                <div className="education-institution">{eBackground.institution}</div>
-                                <div className="education-year">{eBackground.duration}</div>
-                            </div>
-                        )
-                    })
-                }
-            </>
+            {
+                educationBackground &&
+                <>
+                    <div className="resume-body-title"><span className="resume-body-pager"></span>Education</div>
+                    {
+                        educationBackground.map((eBackground, eID) => {
+                            return (
+                                <div className="education-wrapper" key={eID}>
+                                    <div className="education-level">{eBackground.course}</div>
+                                    <div className="education-institution">{eBackground.institution}</div>
+                                    <div className="education-year">{eBackground.duration}</div>
+                                </div>
+                            )
+                        })
+                    }
+                </>
+            }
         </>
     );
 }
