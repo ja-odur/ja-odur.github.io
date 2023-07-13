2a39080125234706928a2e927ac91afede3db7f0
ja-odur <odurjoseph8@gmail.com>
2023-07-13 23:00:46 +0300

    Add gray logo

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -1,21 +1,24 @@
-import logo from '../../logo.svg';
+import logoGray from '../../logo-gray.svg';
+import {Outlet, Link} from "react-router-dom";
 
 
 function NavBar() {
   return (
       <nav className="nav">
         <div className="nav-container">
-          <div className="nav-intro">
-            {/*<img src={logo} className="App-logo" alt="logo" />*/}
-            <svg className="circle" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
-              <path
-                  d="M2 12C2 6.48 6.48 2 12 2s10 4.48 10 10-4.48 10-10 10S2 17.52 2 12zm10 6c3.31 0 6-2.69 6-6s-2.69-6-6-6-6 2.69-6 6 2.69 6 6 6z"></path>
-            </svg>
-            <div className="nav-name">j. a. odur</div>
-          </div>
+          <Link to="/">
+            <div className="nav-intro">
+              <img className="logo-gray" src={logoGray}  alt="logo" />
+              {/*<svg className="circle" focusable="false" viewBox="0 0 24 24" aria-hidden="true">*/}
+              {/*  <path*/}
+              {/*      d="M2 12C2 6.48 6.48 2 12 2s10 4.48 10 10-4.48 10-10 10S2 17.52 2 12zm10 6c3.31 0 6-2.69 6-6s-2.69-6-6-6-6 2.69-6 6 2.69 6 6 6z"></path>*/}
+              {/*</svg>*/}
+              <div className="nav-name">j. a. odur</div>
+            </div>
+          </Link>
           <div className="nav-info">
-            <a href="resume.html" target="_blank">resume</a>
-            <a href="portfolio.html" target="_blank">portfolio</a>
+            <a href="portfolio.html" target="_blank">projects</a>
+            <Link to="/resume" target="_blank">resume</Link>
             <a href="https://github.com/ja-odur" target="_blank">
               github
             </a>
