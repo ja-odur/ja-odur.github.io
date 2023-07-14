2bc252628b166cdf7fb0f95b9efc5aef398adf49
ja-odur <odurjoseph8@gmail.com>
2023-07-15 02:17:49 +0300

    Add projects page cards and sample projects

---
+++ src/components/Projects/data/Projects.js
@@ -0,0 +1,53 @@
+const projects = [
+    {
+        name: "Andrea",
+        image: null,
+        description: "Andrea is an online code compilation platform built with Python/Django and ReactJS. Currently available languages are; C++, Golang, NodsJS and Python",
+        techStacks: "python, golang, docker, postgres, redis",
+        link: "https://github.com/ja-odur/andrea-pub",
+    },
+    {
+        name: "Andrea",
+        image: null,
+        description: "Andrea is an online code compilation platform built with Python/Django and ReactJS. Currently available languages are; C++, Golang, NodsJS and Python",
+        techStacks: "python, golang, docker, postgres, redis",
+        link: "https://github.com/ja-odur/andrea-pub",
+    },
+    {
+        name: "Andrea",
+        image: null,
+        description: "Andrea is an online code compilation platform built with Python/Django and ReactJS. Currently available languages are; C++, Golang, NodsJS and Python",
+        techStacks: "python, golang, docker, postgres, redis",
+        link: "https://github.com/ja-odur/andrea-pub",
+    },
+    {
+        name: "Andrea",
+        image: null,
+        description: "Andrea is an online code compilation platform built with Python/Django and ReactJS. Currently available languages are; C++, Golang, NodsJS and Python",
+        techStacks: "python, golang, docker, postgres, redis",
+        link: "https://github.com/ja-odur/andrea-pub",
+    },
+    {
+        name: "Andrea",
+        image: null,
+        description: "Andrea is an online code compilation platform built with Python/Django and ReactJS. Currently available languages are; C++, Golang, NodsJS and Python",
+        techStacks: "python, golang, docker, postgres, redis",
+        link: "https://github.com/ja-odur/andrea-pub",
+    },
+    {
+        name: "Andrea",
+        image: null,
+        description: "Andrea is an online code compilation platform built with Python/Django and ReactJS. Currently available languages are; C++, Golang, NodsJS and Python",
+        techStacks: "python, golang, docker, postgres, redis",
+        link: "https://github.com/ja-odur/andrea-pub",
+    },
+    {
+        name: "Andrea",
+        image: null,
+        description: "Andrea is an online code compilation platform built with Python/Django and ReactJS. Currently available languages are; C++, Golang, NodsJS and Python",
+        techStacks: "python, golang, docker, postgres, redis",
+        link: "https://github.com/ja-odur/andrea-pub",
+    },
+];
+
+export default projects;
