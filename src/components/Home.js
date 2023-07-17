2a3ce9ed1d38504075d3c2fee0070e96595a6b6f
ja-odur <odurjoseph8@gmail.com>
2023-07-17 13:56:20 +0300

    Add typed home content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,109 @@
 import "../css/Home.css"
+import { useState } from "react";
 import NavBar from "./common/NavBar";
 import Content from "./common/Content";
+import { TypeAnimation } from "react-type-animation";
+
+function TypedGreeting({doneTypingCallback}) {
+    return(
+        <TypeAnimation
+            cursor={false}
+            sequence={[
+                1000,
+                "Hello",
+                500,
+                "Hello 👋",
+                500,
+                "Hello 👋.",
+                800,
+                "Hello 👋,",
+                () => doneTypingCallback && doneTypingCallback()
+            ]}
+            deletionSpeed={90}
+        />
+    )
+}
+
+function TypedIntro() {
+
+    return(
+        <>
+            <TypeAnimation
+                cursor={false}
+                sequence={[
+                    "I'm ",
+                ]}
+            />
+            <span className="text-intro-name">
+                <TypeAnimation
+                    cursor={true}
+                    sequence={[
+                        500,
+                        "Joseph Odur.",
+                        7000,
+                        "J. A. Odur.",
+                        4000,
+                        "Ja-",
+                        500,
+                        "ja-odur.",
+                        3000
+                    ]}
+                    repeat={Infinity}
+                />
+            </span>
+        </>
+    )
+}
+
+function TypedOccupation() {
+    return (
+        <TypeAnimation
+            cursor={false}
+            sequence={[
+                5000,
+                "A Software Engineer,",
+            ]}
+        />
+    )
+}
+
+function TypedMore() {
+    return (
+        <TypeAnimation
+            cursor={false}
+            sequence={[
+                7000,
+                "and human being from Earth!",
+            ]}
+        />
+    )
+}
+
+
 
 function Home() {
-  return (
-    <>
-        <NavBar />
-        <Content>
-            <div className="text-container">
-                <span className="text-greeting">Hello,</span>
-                <span className="text-intro">I'm <span className="text-intro-name">Joseph Odur</span>.</span>
-                <span className="text-occupation">A Software Engineer,</span>
-                <span className="text-more">and human being from Earth!</span>
-            </div>
-        </Content>
-    </>
-  );
+    const [showMainBody, setShowMainBody] = useState(false);
+
+    const showMainBodyCallback = () => setShowMainBody(true);
+
+    return (
+        <>
+            <NavBar />
+            <Content>
+                <div className="text-container">
+                    <span className="text-greeting"><TypedGreeting doneTypingCallback={showMainBodyCallback} /></span>
+                    {
+                       showMainBody &&
+                        <>
+                            <span className="text-intro"><TypedIntro /></span>
+                            <span className="text-occupation"><TypedOccupation /></span>
+                            <span className="text-more"><TypedMore /></span>
+                        </>
+                    }
+                </div>
+            </Content>
+        </>
+    );
 }
 
 export default Home;
